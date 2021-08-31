379704f9816310d1a0a39658669ccc6030250e54
VodzichY <yuri.vodich@gmail.com>
2021-09-01 00:39:10 +0300

    fixin problem by conditions of the task

--- product-service/handler.js
+++ product-service/handler.js
@@ -2,6 +2,22 @@
 
 const { productsList } = require('./products');
 
+module.exports.getProductById = async (event) => {
+  const { pathParameters } = event;
+  const product = productsList.find((elem) => pathParameters.productId === elem.id);
+  if (product === undefined) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({}),
+    };
+  } else {
+    return {
+      statusCode: 200,
+      body: JSON.stringify(product),
+    };
+  }  
+};
+
 module.exports.getProductsList = async (event) => {
   return {
     statusCode: 200,
