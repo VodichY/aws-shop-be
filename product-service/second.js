abd9781fb72878d01a712a15c48034345bea3db7
VodzichY <yuri.vodich@gmail.com>
2021-09-21 00:55:58 +0300

    fixing problem getById endpoint

--- product-service/second.js
+++ product-service/second.js
@@ -1,12 +1,12 @@
 import { App } from '../lib/App';
 
-export const getProductById = async () => {
-  const ProducById = await App.handleSecond();
+export const getProductById = async (event) => {
+  const ProducById = await App.handleSecond(event);
   return {
       statusCode: 200,
       headers: {
         'Content-Type': 'application/json;charset=utf-8'
       },
       body: JSON.stringify(ProducById)
-    }  
+    }
 };
